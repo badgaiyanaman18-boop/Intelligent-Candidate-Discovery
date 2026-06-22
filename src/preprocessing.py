67e9b7494e45d1f533b7892903a470109434ecb9
badgaiyanaman18-boop <badgaiyanaman18@gmail.com>
2026-06-23 00:56:56 +0530

    Add functions to load candidates and job descriptions

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,11 @@
 
+import pandas as pd
+
+def load_candidates(file_path):
+    candidates = pd.read_csv(file_path)
+    return candidates
+
+def load_job_description(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        jd = file.read()
+    return jd
